d31eb218bb4e303b7e801bedafbb710da049d1e5
YousefAyman3444 <zeham3444@gmail.com>
2024-05-10 17:35:40 +0200

    resize feature

---
+++ resize.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef RESIZEFEATURE_H
+#define RESIZEFEATURE_H
+
+
+
+class ResizeFeature {
+public:
+    static void resizeSelectedFigure(CFigure* selectedFigure, double factor);
+};
+
+#endif 
